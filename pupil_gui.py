668b308c9f691d81f8eed41a2722bf37a9c58c74
Yongjae <whdydwo8@gmail.com>
2021-12-17 18:12:21 +0900

    update movie widget [add display and button]

--- pupil_gui.py
+++ pupil_gui.py
@@ -3,51 +3,20 @@ from typing import Union, List, Tuple, Set, Dict
 from PyQt5.QtWidgets import QApplication, QBoxLayout, QSplitter, QWidget, QPushButton, \
                             QToolTip, QMainWindow, QAction, qApp, \
                             QDesktopWidget, QHBoxLayout, QVBoxLayout, \
-                            QFrame, QGridLayout
+                            QFrame, QGridLayout, QLabel
 from PyQt5.QtCore import QCoreApplication, QDate, Qt, QTime
 from PyQt5.QtGui import QFont
 
 
-
-class SetWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-        
-        self.box_layout((1, 0, 1, 0), [self._quitButton()])
-
-    def box_layout(self, position: Tuple[int], widgets: List[QPushButton]):
-        left, right, top, bottom = position
-        
-        hbox = QHBoxLayout()
-        hbox.addStretch(left)
-        for widget in widgets:
-            hbox.addWidget(widget)
-        hbox.addStretch(right)
-        
-        vbox = QVBoxLayout()
-        vbox.addStretch(top)
-        vbox.addLayout(hbox)
-        vbox.addStretch(bottom)
-        self.setLayout(vbox)
-        
-    def _quitButton(self):
-        quit = QPushButton('Quit', self)
-        quit.setToolTip('<b>QPushButton<b> widget')
-        quit.clicked.connect(QCoreApplication.instance().quit)
-        return quit
-    
-
 class pupil(QMainWindow):
     def __init__(self, height=500, width=500):
         super().__init__()
-        self.height = 500
-        self.width = 500
+        self.height = 800
+        self.width = 1300
         
         self.initUI()
-        # self.setCentralWidget(SetWidget())
-        self.setCentralWidget(MainLayout())
-        # self.setLayout(MainLayout())
-
+        self.main_widget = MainWidget()
+        self.setCentralWidget(self.main_widget)
 
     def initUI(self):
         self.setWindowTitle('Pupilometry')
@@ -56,7 +25,7 @@ class pupil(QMainWindow):
         self.statusBar().showMessage('Initialize')
         self._tooltips()
         self._menubar()
-        self._toolbar()
+        # self._toolbar()
         self.show()
 
     def _windowsize(self):
@@ -83,9 +52,9 @@ class pupil(QMainWindow):
         self.fileMenu = self.mainMenu.addMenu('&File')
         self.fileMenu.addAction(self._exitaction())
 
-    def _toolbar(self):
-        self.toolbar = self.addToolBar('Exit')
-        self.toolbar.addAction(self._exitaction(shortcut=False))
+    # def _toolbar(self):
+    #     self.toolbar = self.addToolBar('Exit')
+    #     self.toolbar.addAction(self._exitaction(shortcut=False))
 
     # 카메라가 연결된 경우 동작을 중지하고 끄는것 추가
     def _exitaction(self, shortcut=True):
@@ -96,35 +65,8 @@ class pupil(QMainWindow):
         exitAction.triggered.connect(qApp.quit)
         return exitAction
     
-    # def _widget_layout(self):
-    #     main_layout = QGroupBox('asdf')
 
-    #     video_frame = QFrame()
-    #     video_frame.setFrameShape(QFrame.StyledPanel | QFrame.Raised)
-    #     frame_2 = QFrame()
-    #     frame_2.setFrameShape(QFrame.Panel | QFrame.Sunken)
-        
-    #     layout_1 = QVBoxLayout()
-    #     layout_2 = QVBoxLayout()
-
-    #     button_1 = QPushButton("button_1")
-    #     button_2 = QPushButton("button_2")
-
-    #     layout_1.addWidget(button_1)
-    #     layout_2.addWidget(button_2)
-
-    #     video_frame.setLayout(layout_1)
-    #     frame_2.setLayout(layout_2)
-
-    #     main_layout.addWidget(video_frame)
-    #     main_layout.addWidget(frame_2)
-
-    #     # wg = QWidget()
-    #     # wg.setLayout(main_layout)
-    #     return video_frame
-
-
-class MainLayout(QWidget):
+class MainWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.main_layout = QHBoxLayout()
@@ -136,18 +78,28 @@ class MainLayout(QWidget):
 
         self.setLayout(self.main_layout)
 
-    def _set_movie_frame(self):
+        self._add_movie_widget()
+
+    def _add_movie_widget(self):
+        label1 = QLabel()
+        label1.setText('movie')
+        label2 = QLabel()
+        label2.setText('display')
+        live_bt = QPushButton('live')
+        stop_bt = QPushButton('stop')
+
+        self.movie_layout.addWidget(label1, alignment=Qt.AlignTop)
+        self.movie_layout.addWidget(label2, alignment=Qt.AlignVCenter)
+        self.movie_layout.addWidget(live_bt, alignment=Qt.AlignBottom)
+        self.movie_layout.addWidget(stop_bt, alignment=Qt.AlignBottom)
+
+    def _set_movie_frame(self): 
         '''
         frame for pupil live imaging
         '''
         self.movie_frame = QFrame()
         self.movie_frame.setFrameShape(QFrame.StyledPanel | QFrame.Raised)
-        self.movie_layout = QGridLayout()
-        
-        # add widget in this section, tmp
-        button_1 = QPushButton("button_1")
-        self.movie_layout.addWidget(button_1)
-
+        self.movie_layout = QHBoxLayout()
         self.movie_frame.setLayout(self.movie_layout)
 
     def _set_graph_frame(self):
@@ -157,22 +109,12 @@ class MainLayout(QWidget):
         self.graph_frame = QFrame()
         self.graph_frame.setFrameShape(QFrame.StyledPanel | QFrame.Raised)
         self.graph_layout = QGridLayout()
-
-        # add widget in this section, tmp
-        button_2 = QPushButton("button_2")
-        self.graph_layout.addWidget(button_2)
-
         self.graph_frame.setLayout(self.graph_layout)
 
     def _set_interactive_frmae(self):
         self.interactive_frame = QFrame()
         self.interactive_frame.setFrameShape(QFrame.StyledPanel | QFrame.Raised)
         self.interactive_layout = QHBoxLayout()
-
-        # add widget in this section, tmp
-        button_3 = QPushButton("button_3")
-        self.interactive_layout.addWidget(button_3)
-
         self.interactive_frame.setLayout(self.interactive_layout)
 
     def _main_division(self):
@@ -181,17 +123,17 @@ class MainLayout(QWidget):
         '''
         self.splt1 = QSplitter(Qt.Vertical)
         self.splt2 = QSplitter(Qt.Horizontal)  
-
+        
         self.splt1.addWidget(self.movie_frame)
         self.splt1.addWidget(self.graph_frame)
         self.splt2.addWidget(self.splt1)
         self.splt2.addWidget(self.interactive_frame)
+        
+        self.splt2.setSizes([400, 100])
+        self.splt1.setSizes([100, 100])
+        self.main_layout.addWidget(self.splt2)
 
         
-
-        self.main_layout.addWidget(self.splt2)
-        self.splt1.setSizes([1, 1])
-        self.splt2.setSizes([3, 1])
         
 if __name__=='__main__':
     app = QApplication(sys.argv)
