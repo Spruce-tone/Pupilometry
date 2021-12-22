51eaaa97f698b24b183788679db258246e8bdab0
Yongjae <whdydwo8@gmail.com>
2021-12-22 18:09:14 +0900

    Add live, pasuse imaging button

--- lib/SignalConnection.py
+++ lib/SignalConnection.py
@@ -1,51 +1,56 @@
-from PyQt5.QtCore import pyqtSignal, QThread
+from PyQt5.QtCore import pyqtSignal, QThread, Qt
 from PyQt5.QtGui import QImage
 from lib import tisgrabber as tis
 import numpy as np
 import ctypes
 
 
-ic = ctypes.cdll.LoadLibrary('./lib/tisgrabber_x64.dll')
-
-
 class LiveDisplay(QThread):
     Pixmap_display = pyqtSignal(QImage)
+    
+    def __init__(self, parent, camera, ic):
+        super().__init__(parent)
+        self.camera = camera
+        self.ic = ic
+
+        self.Width = ctypes.c_long()
+        self.Height = ctypes.c_long()
+        self.BitsPerPixel = ctypes.c_int()
+        self.colorformat = ctypes.c_int()
+
+        self.running = True
 
     def run(self):
-        tis.declareFunctions(ic)
-        ic.IC_InitLibrary(0)
-        hGrabber = tis.openDevice(ic)
+        self.ic.IC_StartLive(self.camera, 0)
+        while self.running:
+            if self.ic.IC_SnapImage(self.camera, 2000) == tis.IC_SUCCESS:
+                # Query the values
+                self.ic.IC_GetImageDescription(self.camera, self.Width, self.Height, self.BitsPerPixel, self.colorformat)
 
-        ic.IC_StartLive(hGrabber, 0)
+                 # Calculate the buffer size
+                self.bpp = int(self.BitsPerPixel.value / 8.0 )
+                self.buffer_size = self.Width.value * self.Height.value * self.BitsPerPixel.value
 
-        Width = ctypes.c_long()
-        Height = ctypes.c_long()
-        BitsPerPixel = ctypes.c_int()
-        colorformat = ctypes.c_int()
-        # Query the values
-        ic.IC_GetImageDescription( hGrabber, Width, Height, BitsPerPixel, colorformat )
-
-        # Calculate the buffer size
-        bpp = int( BitsPerPixel.value / 8.0 )
-        buffer_size = Width.value * Height.value * BitsPerPixel.value
-
-        while True:
-            if ic.IC_SnapImage(hGrabber, 2000) == tis.IC_SUCCESS:
-        
                 # Get the image data
-                imagePtr =  ic.IC_GetImagePtr(hGrabber)
+                imagePtr =  self.ic.IC_GetImagePtr(self.camera)
 
-                imagedata = ctypes.cast(imagePtr, ctypes.POINTER(ctypes.c_ubyte * buffer_size))
+                imagedata = ctypes.cast(imagePtr, ctypes.POINTER(ctypes.c_ubyte * self.buffer_size))
 
                 # Create the numpy array
                 img = np.ndarray(buffer = imagedata.contents,
                         dtype = np.uint8,
-                        shape = (Height.value,
-                                Width.value,
-                                bpp))
+                        shape = (self.Height.value,
+                                self.Width.value,
+                                self.bpp))
 
                 # correct channel order                                
-                img[:, :, :] = img[:, :, [2, 1, 0]]
+                img[:, :, :] = img[:, :, ::-1]
 
                 qimage = QImage(img.data, img.shape[1], img.shape[0], img.strides[0],  QImage.Format_RGB888)
                 self.Pixmap_display.emit(qimage)
+        
+    def resume(self):
+        self.running = True
+    
+    def pause(self):
+        self.running = False
--- pupil_gui.py
+++ pupil_gui.py
@@ -4,21 +4,22 @@ from typing import Union, List, Tuple, Set, Dict
 from PyQt5.QtWidgets import QApplication, QBoxLayout, QSplitter, QWidget, QPushButton, \
                             QToolTip, QMainWindow, QAction, qApp, \
                             QDesktopWidget, QHBoxLayout, QVBoxLayout, \
-                            QFrame, QGridLayout, QLabel
+                            QFrame, QGridLayout, QLabel, QGroupBox
 from PyQt5.QtCore import QCoreApplication, QDate, Qt, QTime, pyqtSignal, QThread, pyqtSlot
 from PyQt5.QtGui import QFont, QPixmap, QImage
 from lib import tisgrabber as tis
 import numpy as np
 import ctypes
 
+from lib.SignalConnection import LiveDisplay
 
 
 
 class pupil(QMainWindow):
     def __init__(self, height=500, width=500):
         super().__init__()
-        self.height = 1000
-        self.width = 1400
+        self.height = 1400
+        self.width = 1600
         
         # define main widget
         self.main_widget = MainWidget()
@@ -111,6 +112,7 @@ class MainWidget(QWidget):
         self.setLayout(self.main_layout)
 
         self._add_movie_widget()
+        self._add_control_panel_widget()
 
     def _init_camera(self):
         '''
@@ -123,18 +125,47 @@ class MainWidget(QWidget):
 
     # Movie widget 만들기
     def _add_movie_widget(self):
-        movie_status = QLabel('Pupil display')
-        movie_status.setFont(QFont('Arial', 15))
-        self.movie_layout.addWidget(movie_status, alignment=Qt.AlignTop)
         self.label = QLabel(self)
-        # th = Thread()
-        # th.start()
-        # th.Pixmap_display.connect(self.display_image)
-        # print('asdf')
-        # self.movie_layout.addWidget(label2, alignment=Qt.AlignTop)
-        self.movie_layout.addWidget(self.label, alignment=Qt.AlignVCenter)
-        # self.movie_layout.addWidget(live_bt, alignment=Qt.AlignBottom)
-        # self.movie_layout.addWidget(stop_bt, alignment=Qt.AlignBottom)
+        self.label.setScaledContents(True)
+        self.label.resize(720, 1080)
+
+        self.live = LiveDisplay(self, self.camera, self.ic)
+        self.live.start()
+        self.live.Pixmap_display.connect(self.display_image)  
+        self.movie_layout.addWidget(self.label)
+
+    # control pannel widget
+    def _add_control_panel_widget(self):
+        self.state_panel = QGroupBox('Status')
+        self.state_panel.setFont(QFont('Arial', 10))
+
+
+        self.file_panel = QGroupBox('Files')
+        self.file_panel.setFont(QFont('Arial', 10))
+
+        # imaging control
+        self.imaging_control = QGroupBox('Imaging control')
+        self.imaging_control.setFont(QFont('Arial', 10))
+        self.imaging_control_layout = QVBoxLayout()
+
+        live_imaging = QPushButton('Live')
+        stop_imaging = QPushButton('Stop')
+        ready_trigger = QPushButton('Set trigger')
+        self._add_multiple_widget(self.imaging_control_layout, [live_imaging, stop_imaging, ready_trigger])
+        self.imaging_control.setLayout(self.imaging_control_layout)
+
+        live_imaging.clicked.connect(self._resume_live_imaging)
+        stop_imaging.clicked.connect(self._stop_live_imaging)
+        
+
+        # Control panel ratio
+        self.control_panel_layout.addWidget(self.state_panel, 2)
+        self.control_panel_layout.addWidget(self.file_panel, 4)
+        self.control_panel_layout.addWidget(self.imaging_control, 2)
+
+    def _add_multiple_widget(self, layout : QBoxLayout, widgets : List):
+        for widget in widgets:
+            layout.addWidget(widget)
 
     def _set_frame(self, layout):
         '''
@@ -150,9 +181,9 @@ class MainWidget(QWidget):
         '''
         generate frames to use
         '''
-        self.movie_frame, self.movie_layout = self._set_frame(QHBoxLayout())
+        self.movie_frame, self.movie_layout = self._set_frame(QGridLayout())
         self.graph_frame, self.graph_layout = self._set_frame(QGridLayout())
-        self.interactive_frame, self.interactive_layout = self._set_frame(QHBoxLayout())
+        self.control_panel_frame, self.control_panel_layout = self._set_frame(QVBoxLayout())
 
     def _main_division(self):
         '''
@@ -164,24 +195,36 @@ class MainWidget(QWidget):
         self.splt1.addWidget(self.movie_frame)
         self.splt1.addWidget(self.graph_frame)
         self.splt2.addWidget(self.splt1)
-        self.splt2.addWidget(self.interactive_frame)
+        self.splt2.addWidget(self.control_panel_frame)
         
         self.splt2.setSizes([250, 100])
         self.splt1.setSizes([100, 100])
         self.main_layout.addWidget(self.splt2)
     
     @pyqtSlot(QImage)
-    def display_image(self, image):
-        self.label.setPixmap(QPixmap.fromImage(image))
+    def display_image(self, qimage):
+        self.live_pixmap = QPixmap.fromImage(qimage)
+        self.live_pixmap.scaled(720, 470, Qt.KeepAspectRatioByExpanding)
+        self.label.setPixmap(self.live_pixmap)
+    
+    def _resume_live_imaging(self):
+        self.live.resume()
+        self.live.start()
 
-
-
-
-
-
+    def _stop_live_imaging(self):
+        self.live.pause()
 
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
     ex = pupil()
     sys.exit(app.exec_())
+
+
+         # movie_status = QLabel('Pupil display')
+        # movie_status.setFont(QFont('Arial', 15))
+        # self.movie_layout.addWidget(movie_status, alignment=Qt.AlignHCenter)
+        # self.movie_layout.addWidget(label2, alignment=Qt.AlignTop)
+        # self.movie_layout.addWidget(self.label, alignment=Qt.AlignHCenter)
+        # self.movie_layout.addWidget(live_bt, alignment=Qt.AlignBottom)
+        # self.movie_layout.addWidget(stop_bt, alignment=Qt.AlignBottom)
