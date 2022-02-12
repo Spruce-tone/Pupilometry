de1e0dcaa53e63b09540d865b10350d8537f0e96
Yongjae <whdydwo8@gmail.com>
2022-02-12 21:37:33 +0900

    [In progress] try to build .exe file

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,43 @@
-FROM ubuntu:20.04
-RUN apt-get -y update
-RUN apt-get -y install python3.8
-RUN apt-get -y install python3-pip
-# These commands install the cv2 dependencies that are normally present on the local machine, but might be missing in your Docker container causing the issue.
-RUN apt-get install ffmpeg libsm6 libxext6  -y
-RUN pip3 install numpy opencv-python scikit-image PyQt5 pyinstaller tensorflow-gpu==2.7
+# FROM ubuntu:20.04
+# RUN apt-get -y update 
+# RUN apt-get -y install python3.7 
+# RUN apt-get -y install python3-pip
+#     # These commands install the cv2 dependencies that are normally present on the local machine, but might be missing in your Docker container causing the issue.
+# RUN apt-get install ffmpeg libsm6 libxext6  -y
+# RUN pip3 install "deeplabcut[gui]>=2.2.0.2" deeplabcut-live numpy==1.19.5 decorator==4.4.2 tensorflow==2.5.0 PyQt5 scikit-image pyqtgraph  opencv-python 
+
+# FROM deeplabcut/deeplabcut:base
+
+# RUN DEBIAN_FRONTEND=noninteractive apt-get update -yy \ 
+#     && DEBIAN_FRONTEND=noninteractive \
+#          apt-get install -yy --no-install-recommends \
+#          libgtk-3-dev python3-wxgtk4.0 locales \
+#     && apt-get clean \
+#     && rm -rf /var/lib/apt/lists/* \
+#     && locale-gen en_US.UTF-8 en_GB.UTF-8
+
+# RUN pip install --no-cache-dir --upgrade pip \
+#  && pip install --no-cache-dir --upgrade "deeplabcut[gui]>=2.2.0.2" deeplabcut-live numpy==1.19.5 decorator==4.4.2 tensorflow==2.5.0 PyQt5 scikit-image pyqtgraph  opencv-python 
+
+# ENV DLClight=False
+# # CMD ["cd", "data"]
+# # CMD ["python3", "-m", "FlyingSesame.py"]
+
+# -------------
+FROM continuumio/miniconda3:latest
+
+RUN apt-get update -y; apt-get upgrade -y
+RUN apt-get update -y; apt-get upgrade -y; apt-get install -y vim-tiny vim-athena ssh
+
+COPY environment.yml environment.yml
+
+RUN conda env create -f environment.yml
+RUN echo "alias l='ls -lah'" >> ~/.bashrc
+RUN echo "source activate coin" >> ~/.bashrc
+
+ENV CONDA_EXE /opt/conda/bin/conda
+ENV CONDA_PREFIX /opt/conda/envs/livepupil4
+ENV CONDA_PYTHON_EXE /opt/conda/bin/python
+ENV CONDA_PROMPT_MODIFIER (livepupil4)
+ENV CONDA_DEFAULT_ENV livepupil4
+ENV PATH /opt/conda/envs/livepupil4/bin:/opt/conda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ from cx_Freeze import setup, Executable
 # "packages": ["os"] is used as example only
 include_files = ['./lib', 
                 (os.path.join(os.environ["CONDA_PREFIX"], 'DLLs', 'tcl86t.dll'), os.path.join('lib', 'tcl86t.dll')),
-                (os.path.join(os.environ["CONDA_PREFIX"], 'DLLs', 'tk86t.dll'), os.path.join('lib', 'tk86t.dll')),
-                (os.path.join(os.environ["CONDA_PREFIX"], 'Library', 'bin', 'mkl_intel_thread.1.dll'), os.path.join('lib', 'mkl_intel_thread.1.dll'))]
-build_exe_options = {'packages': ['numpy'], 'excludes': [], 'include_files' : include_files}
+                (os.path.join(os.environ["CONDA_PREFIX"], 'DLLs', 'tk86t.dll'), os.path.join('lib', 'tk86t.dll'))]
+build_exe_options = {'packages': ['numpy', 'pip', 'keras'], 'excludes': [], 'include_files' : include_files}
 
 
 # base="Win32GUI" should be used only for Windows GUI app
@@ -25,51 +24,4 @@ setup(
     # executables = [Executable("guifoo.py", base=base)]
     executables = exe
 )
-
-# -*- mode: python ; coding: utf-8 -*-
-from PyInstaller.utils.hooks import collect_data_files
-import os
-
-block_cipher = None
-
-
-a = Analysis(['FlyingSesame.py'],
-             pathex=['.',
-             '.\\lib',
-             '.\\lib\\Automation'],
-             binaries=[('lib\\tisgrabber_x64.dll', '.'),
-                        ('lib\\TIS.Imaging.ICImagingControl35.dll', '.'),
-                        ('lib\\TIS_UDSHL11_x64.dll', '.')],
-             datas=[('C:\\Users\\spruce\\anaconda3\\envs\\livepupil2\\Lib\\site-packages\\tensorflow\\python\\tensorflow.python._pywrap_tensorflow_internal.pyd', '.')],
-             hiddenimports=['tensorflow',
-                            'tensorflow.python._pywrap_tensorflow_internal',
-                            'sklearn.neighbors.typedefs',
-                            'sklearn.neighbors.quad_tree',
-                            'sklearn.tree',
-                            'sklearn.tree._utils',
-                            'tensorflow._api.v2.compat.v1.compat.v1.keras.datasets.fashion_mnist'],
-             hookspath=[],
-             runtime_hooks=[],
-             excludes=[],
-             win_no_prefer_redirects=False,
-             win_private_assemblies=False,
-             cipher=block_cipher,
-             noarchive=False)
-pyz = PYZ(a.pure, a.zipped_data,
-             cipher=block_cipher)
-
-exe = EXE(pyz,
-          a.scripts,
-          a.binaries,
-          a.zipfiles,
-          a.datas,
-          [],
-          name='FlyingSesame',
-          debug=False,
-          bootloader_ignore_signals=False,
-          strip=False,
-          upx=True,
-          upx_exclude=[],
-          runtime_tmpdir=None,
-          console=True )
 
